840cbb044d650b84b8c90382bf443089942a4c1d
YulianKuryliak <nimpha14590@gmail.com>
2025-09-03 12:04:46 +0300

    Structure update

---
+++ labs/Python/lab_0/solution.py
@@ -0,0 +1,12 @@
+from Py_mod import py_mod
+
+
+py_mod.test_print("123")
+
+
+def greet(name):
+    return "Hello, " + name
+
+
+def add(a: int, b: int) -> int:
+    return a + b
---
+++ src/Py_mod/py_mod.py
@@ -0,0 +1,6 @@
+def greet():
+    print("The head of the project is Yulian")
+
+
+def test_print(s: str):
+    print("test", s)
